ece31976505ea31180dd2d7acb4fcbd507dd1519
zhang <2474942479@qq.com>
2021-04-20 11:49:55 +0800

    格式化

--- common/service_base/src/main/java/edu/zsq/servicebase/Knife4jConfiguration.java
+++ common/service_base/src/main/java/edu/zsq/servicebase/Knife4jConfiguration.java
@@ -20,25 +20,26 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class Knife4jConfiguration {
 
-            @Bean
-            public Docket webApiConfig(){
-                return new Docket(DocumentationType.SWAGGER_2)
-                        .groupName("online_edu")
-                        .apiInfo(webApiInfo())
-                        .select()
-                        //这里指定Controller扫描包路径
+    @Bean
+    public Docket webApiConfig() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("online_edu")
+                .apiInfo(webApiInfo())
+                .select()
+                //这里指定Controller扫描包路径
 //                        .apis(RequestHandlerSelectors.basePackage("com.github.xiaoymin.knife4j.controller"))
 //                        .paths(Predicates.not(PathSelectors.regex("/admin/.*")))
-                        .paths(PathSelectors.any())
-                        .build();
-            }
-                private ApiInfo webApiInfo(){
-                    return new ApiInfoBuilder()
-                            .title("假老外外语学习平台API文档")
-                            .description("本文档描述了假老外外语学习平台微服务接口定义")
-                            .version("1.0")
-                            .contact(new Contact("张淞淇", "http://baidu.com", "2474942479@qq.com"))
-                            .build();
-                }
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo webApiInfo() {
+        return new ApiInfoBuilder()
+                .title("假老外外语学习平台API文档")
+                .description("本文档描述了假老外外语学习平台微服务接口定义")
+                .version("1.0")
+                .contact(new Contact("张淞淇", "http://baidu.com", "2474942479@qq.com"))
+                .build();
+    }
 
 }
